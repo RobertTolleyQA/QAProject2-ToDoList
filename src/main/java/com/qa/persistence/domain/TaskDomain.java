8a358197adf6910528b914aa0e44274732ae2311
RobTolley <rtolley@qa.com>
2021-02-04 16:05:58 +0000

    ADD: TaskDomain - base complete ALTER: dev.properties schema commented out

---
+++ src/main/java/com/qa/persistence/domain/TaskDomain.java
@@ -0,0 +1,82 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class TaskDomain {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String name;
+	
+	private String desc;
+	
+	private Double estTime;
+	
+	private Integer estWorkers;
+
+	public TaskDomain(Long id, @NotNull String name, String desc, Double estTime, Integer estWorkers) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.desc = desc;
+		this.estTime = estTime;
+		this.estWorkers = estWorkers;
+	}
+
+	public TaskDomain() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public Double getEstTime() {
+		return estTime;
+	}
+
+	public void setEstTime(Double estTime) {
+		this.estTime = estTime;
+	}
+
+	public Integer getEstWorkers() {
+		return estWorkers;
+	}
+
+	public void setEstWorkers(Integer estWorkers) {
+		this.estWorkers = estWorkers;
+	}
+	
+	
+	
+	
+
+}
